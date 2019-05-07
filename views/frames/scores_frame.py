9c165da96c268f6c3d54e6a112fd6558986576c3
Madeorsk <madeorsk@protonmail.com>
2019-05-07 18:09:12 +0200

    Vue : Affichage du nom du / des gagnant(s).

--- views/frames/scores_frame.py
+++ views/frames/scores_frame.py
@@ -9,6 +9,10 @@ class ScoresFrame(Frame):
         self.message = Label(self, textvariable=self.result, font=("Helvetica", 30), fg='lightblue')
         self.message.pack(side="top", pady=(60, 30))
 
+        self.winner_name_label_var = StringVar(value="")
+        self.winner_name_label = Label(self, textvariable=self.winner_name_label_var, font=("Helvetica", 16))
+        self.winner_name_label.pack(side="top", pady=(0, 30))
+
         Button(self, text="Quitter", command=master.stop).pack()
 
     def init(self):
@@ -16,7 +20,15 @@ class ScoresFrame(Frame):
             if self.master.winners[0] == self.master.player_name:
                 self.result.set("Vous avez gagné !")
                 self.message.config(fg='gold')
+            else:
+                self.winner_name_label_var.set(self.master.winners[0] + " a gagné !")
         else:
             if self.master.player_name in self.master.winners:
                 self.result.set("Égalité !")
                 self.message.config(fg='orange')
+            
+            winners_label_content = "Les gagnants sont : "
+            for winner_name in self.master.winners:
+                winners_label_content += winner_name + ", "
+            self.winner_name_label_var.set(winners_label_content[:-2])
+
